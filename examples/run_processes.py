8624c7f7c1d2622af5149a2d74af26a601decb0c
viderman <viderman@yahooinc.com>
2023-09-27 15:52:33 +0300

    refactoring

--- examples/run_processes.py
+++ examples/run_processes.py
@@ -10,11 +10,28 @@ emb_sizes = [4, 8]
 device_inds = list(range(4)) * 2
 
 # criteo
-all_options_for_studies = [('lowrank_fwfm', 'logloss', 1, 4), ('lowrank_fwfm', 'logloss', 1, 8), ('lowrank_fwfm', 'logloss', 2, 4), ('lowrank_fwfm', 'logloss', 2, 8), ('lowrank_fwfm', 'logloss', 3, 4), ('lowrank_fwfm', 'logloss', 3, 8), ('lowrank_fwfm', 'logloss', 4, 4), ('lowrank_fwfm', 'logloss', 4, 8), ('lowrank_fwfm', 'logloss', 5, 4), ('lowrank_fwfm', 'logloss', 5, 8), ('lowrank_fwfm', 'auc', 1, 4), ('lowrank_fwfm', 'auc', 1, 8), ('lowrank_fwfm', 'auc', 2, 4), ('lowrank_fwfm', 'auc', 2, 8), ('lowrank_fwfm', 'auc', 3, 4), ('lowrank_fwfm', 'auc', 3, 8), ('lowrank_fwfm', 'auc', 4, 4), ('lowrank_fwfm', 'auc', 4, 8), ('lowrank_fwfm', 'auc', 5, 4), ('lowrank_fwfm', 'auc', 5, 8), ('pruned_fwfm', 'logloss', 1, 4), ('pruned_fwfm', 'logloss', 1, 8), ('pruned_fwfm', 'logloss', 2, 4), ('pruned_fwfm', 'logloss', 2, 8), ('pruned_fwfm', 'logloss', 3, 4), ('pruned_fwfm', 'logloss', 3, 8), ('pruned_fwfm', 'logloss', 4, 4), ('pruned_fwfm', 'logloss', 4, 8), ('pruned_fwfm', 'logloss', 5, 4), ('pruned_fwfm', 'logloss', 5, 8), ('pruned_fwfm', 'auc', 1, 4), ('pruned_fwfm', 'auc', 1, 8), ('pruned_fwfm', 'auc', 2, 4), ('pruned_fwfm', 'auc', 2, 8), ('pruned_fwfm', 'auc', 3, 4), ('pruned_fwfm', 'auc', 3, 8), ('pruned_fwfm', 'auc', 4, 4), ('pruned_fwfm', 'auc', 4, 8), ('pruned_fwfm', 'auc', 5, 4), ('pruned_fwfm', 'auc', 5, 8), ('fwfm', 'logloss', 0, 4), ('fwfm', 'logloss', 0, 8), ('fwfm', 'auc', 0, 4), ('fwfm', 'auc', 0, 8)]
-lst_michael = all_options_for_studies[0:20]
-lst_oren = all_options_for_studies[20:28]
-lst_ariel = all_options_for_studies[28:36]
+def generate_all_criteo_avazu_options():
+    all_options_for_studies = [(m_to_check, met_to_opt, rank, emb_size) for m_to_check in [lowrank_fwfm, pruned_fwfm]
+                               for met_to_opt in metrics_to_optimize for rank in ranks_to_check for emb_size in emb_sizes]
+    all_options_for_studies_fwfm = [(fwfm, met_to_opt, 0, emb_size) for met_to_opt in metrics_to_optimize for emb_size
+                                    in emb_sizes]
+    all_option_for_studies_fm = [(fm, met_to_opt, 0, emb_size) for met_to_opt in metrics_to_optimize for emb_size in emb_sizes]
+    all_options_for_studies.extend(all_options_for_studies_fwfm)
+    all_options_for_studies.extend(all_option_for_studies_fm)
+    return all_options_for_studies
+
+all_options_for_studies = [('lowrank_fwfm', 'logloss', 1, 4), ('lowrank_fwfm', 'logloss', 1, 8), ('lowrank_fwfm', 'logloss', 2, 4), ('lowrank_fwfm', 'logloss', 2, 8), ('lowrank_fwfm', 'logloss', 3, 4), ('lowrank_fwfm', 'logloss', 3, 8), ('lowrank_fwfm', 'logloss', 4, 4), ('lowrank_fwfm', 'logloss', 4, 8), ('lowrank_fwfm', 'logloss', 5, 4), ('lowrank_fwfm', 'logloss', 5, 8), ('lowrank_fwfm', 'auc', 1, 4), ('lowrank_fwfm', 'auc', 1, 8), ('lowrank_fwfm', 'auc', 2, 4), ('lowrank_fwfm', 'auc', 2, 8), ('lowrank_fwfm', 'auc', 3, 4), ('lowrank_fwfm', 'auc', 3, 8), ('lowrank_fwfm', 'auc', 4, 4), ('lowrank_fwfm', 'auc', 4, 8), ('lowrank_fwfm', 'auc', 5, 4), ('lowrank_fwfm', 'auc', 5, 8), ('pruned_fwfm', 'logloss', 1, 4), ('pruned_fwfm', 'logloss', 1, 8), ('pruned_fwfm', 'logloss', 2, 4), ('pruned_fwfm', 'logloss', 2, 8), ('pruned_fwfm', 'logloss', 3, 4), ('pruned_fwfm', 'logloss', 3, 8), ('pruned_fwfm', 'logloss', 4, 4), ('pruned_fwfm', 'logloss', 4, 8), ('pruned_fwfm', 'logloss', 5, 4), ('pruned_fwfm', 'logloss', 5, 8), ('pruned_fwfm', 'auc', 1, 4), ('pruned_fwfm', 'auc', 1, 8), ('pruned_fwfm', 'auc', 2, 4), ('pruned_fwfm', 'auc', 2, 8), ('pruned_fwfm', 'auc', 3, 4), ('pruned_fwfm', 'auc', 3, 8), ('pruned_fwfm', 'auc', 4, 4), ('pruned_fwfm', 'auc', 4, 8), ('pruned_fwfm', 'auc', 5, 4), ('pruned_fwfm', 'auc', 5, 8), ('fwfm', 'logloss', 0, 4), ('fwfm', 'logloss', 0, 8), ('fwfm', 'auc', 0, 4), ('fwfm', 'auc', 0, 8), ('fm', 'logloss', 0, 4), ('fm', 'logloss', 0, 8), ('fm', 'auc', 0, 4), ('fm', 'auc', 0, 8)]
+lst_michael = all_options_for_studies[0:12]
+lst_oren = all_options_for_studies[12:24]
+lst_ariel = all_options_for_studies[24:36]
 lst_naama = all_options_for_studies[36:]
+
+
+# all_options_for_studies = [('lowrank_fwfm', 'logloss', 1, 4), ('lowrank_fwfm', 'logloss', 1, 8), ('lowrank_fwfm', 'logloss', 2, 4), ('lowrank_fwfm', 'logloss', 2, 8), ('lowrank_fwfm', 'logloss', 3, 4), ('lowrank_fwfm', 'logloss', 3, 8), ('lowrank_fwfm', 'logloss', 4, 4), ('lowrank_fwfm', 'logloss', 4, 8), ('lowrank_fwfm', 'logloss', 5, 4), ('lowrank_fwfm', 'logloss', 5, 8), ('lowrank_fwfm', 'auc', 1, 4), ('lowrank_fwfm', 'auc', 1, 8), ('lowrank_fwfm', 'auc', 2, 4), ('lowrank_fwfm', 'auc', 2, 8), ('lowrank_fwfm', 'auc', 3, 4), ('lowrank_fwfm', 'auc', 3, 8), ('lowrank_fwfm', 'auc', 4, 4), ('lowrank_fwfm', 'auc', 4, 8), ('lowrank_fwfm', 'auc', 5, 4), ('lowrank_fwfm', 'auc', 5, 8), ('pruned_fwfm', 'logloss', 1, 4), ('pruned_fwfm', 'logloss', 1, 8), ('pruned_fwfm', 'logloss', 2, 4), ('pruned_fwfm', 'logloss', 2, 8), ('pruned_fwfm', 'logloss', 3, 4), ('pruned_fwfm', 'logloss', 3, 8), ('pruned_fwfm', 'logloss', 4, 4), ('pruned_fwfm', 'logloss', 4, 8), ('pruned_fwfm', 'logloss', 5, 4), ('pruned_fwfm', 'logloss', 5, 8), ('pruned_fwfm', 'auc', 1, 4), ('pruned_fwfm', 'auc', 1, 8), ('pruned_fwfm', 'auc', 2, 4), ('pruned_fwfm', 'auc', 2, 8), ('pruned_fwfm', 'auc', 3, 4), ('pruned_fwfm', 'auc', 3, 8), ('pruned_fwfm', 'auc', 4, 4), ('pruned_fwfm', 'auc', 4, 8), ('pruned_fwfm', 'auc', 5, 4), ('pruned_fwfm', 'auc', 5, 8), ('fwfm', 'logloss', 0, 4), ('fwfm', 'logloss', 0, 8), ('fwfm', 'auc', 0, 4), ('fwfm', 'auc', 0, 8)]
+# lst_michael = all_options_for_studies[0:20]
+# lst_oren = all_options_for_studies[20:28]
+# lst_ariel = all_options_for_studies[28:36]
+# lst_naama = all_options_for_studies[36:]
 
 # FM
 fm_options = [(fm, met_to_opt, 0, emb_size) for met_to_opt in metrics_to_optimize for emb_size in emb_sizes]
@@ -38,12 +55,8 @@ movielens_options_studies = generate_movielens_options()
 
 if __name__ == '__main__':
 
-    #all_options_for_studies = [(m_to_check, met_to_opt, rank, emb_size) for m_to_check in [lowrank_fwfm, pruned_fwfm] for met_to_opt in metrics_to_optimize for rank in criteo_ranks for emb_size in emb_sizes]
-    #all_options_for_studies_fwfm = [(fwfm, met_to_opt, 0, emb_size) for met_to_opt in metrics_to_optimize for emb_size in emb_sizes]
-    #all_options_for_studies.extend(all_options_for_studies_fwfm)
-
     queue = mp.Queue()
-    for tpl in movielens_options_studies:
+    for tpl in lst_michael:
         queue.put(tpl)
 
     processes = [Process(target=run_all_for_device_ind, args=(queue, dev_ind), daemon=True) for dev_ind in device_inds]
