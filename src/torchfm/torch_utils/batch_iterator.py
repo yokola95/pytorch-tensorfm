367f321a8e7ad1a0656384e70c65a8af5969601e
viderman <viderman@yahooinc.com>
2024-03-07 11:52:08 +0000

    minor change

--- src/torchfm/torch_utils/batch_iterator.py
+++ src/torchfm/torch_utils/batch_iterator.py
@@ -7,13 +7,13 @@ def batch_iter_dataset(dataset, device, batch_size=2048, shuffle=True):
     dataset: features and labels dataset
     """
     size_data = len(dataset)
-    items_tensor = torch.tensor(dataset.items)  #, device=device)       # feature values
-    targets_tensor = torch.tensor(dataset.targets)  #, device=device)   # labels
+    items_tensor = torch.tensor(dataset.items, device=device)       # feature values
+    targets_tensor = torch.tensor(dataset.targets, device=device)   # labels
 
     if shuffle:
-        idxs = torch.randperm(size_data)  #, device=device)
+        idxs = torch.randperm(size_data, device=device)
     else:
-        idxs = torch.arange(size_data)  # , device=device)
+        idxs = torch.arange(size_data, device=device)
 
     for batch_idxs in idxs.split(batch_size):
         yield items_tensor[batch_idxs], targets_tensor[batch_idxs]
