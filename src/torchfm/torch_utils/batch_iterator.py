3116382e6311a38a69b5806457eaccde82a1dcdc
viderman <viderman@yahooinc.com>
2024-04-09 18:18:40 +0100

    remove redundant (commented out) lines

--- src/torchfm/torch_utils/batch_iterator.py
+++ src/torchfm/torch_utils/batch_iterator.py
@@ -2,25 +2,6 @@ import torch
 import numpy as np
 
 
-#
-#
-# def batch_iter_dataset(dataset, device, batch_size=2048, shuffle=True):
-#     """
-#     dataset: features and labels dataset
-#     """
-#     size_data = len(dataset)
-#     items_tensor = torch.tensor(dataset.items, device=device)       # feature values
-#     targets_tensor = torch.tensor(dataset.targets, device=device)   # labels
-#
-#     if shuffle:
-#         idxs = torch.randperm(size_data, device=device)
-#     else:
-#         idxs = torch.arange(size_data, device=device)
-#
-#     for batch_idxs in idxs.split(batch_size):
-#         yield items_tensor[batch_idxs], targets_tensor[batch_idxs]
-
-
 class BatchIter:
     def __init__(self, dataset, device, batch_size=1024, shuffle=True):
         self.multivalued = dataset.multivalued
@@ -40,16 +21,6 @@ class BatchIter:
                     torch.tensor(dataset.offsets, device=device),
                     torch.tensor(dataset.weights, device=device))
 
-    # def batches(self):
-    #     if self.shuffle:
-    #         # should be deterministic if torch.manual_seed(...) is set
-    #         idxs = torch.randperm(self.size_data, device=self.device)
-    #     else:
-    #         idxs = torch.arange(self.size_data, device=self.device)
-    #
-    #     for batch_idxs in idxs.split(self.batch_size):
-    #         yield self.items_tensors[batch_idxs], self.targets_tensor[batch_idxs]
-
     def __len__(self):
         return self.size_data
 
@@ -66,13 +37,4 @@ class BatchIter:
             tmp_items_tensors = tuple((x[batch_idxs, ...] for x in self.items_tensors))
             tmp_targets_tensor = self.targets_tensor[batch_idxs]
             yield tmp_items_tensors, tmp_targets_tensor
-
-# class BatchIterMultiValued(BatchIter):
-#     def __init__(self, dataset, device, batch_size=1024, shuffle=True):
-#         super().__init__(dataset, device, batch_size, shuffle)
-#
-#     def get_items_tensor(self, dataset, device):
-#         np_items = np.concatenate((dataset.indices, dataset.offsets, dataset.weights), axis=1)
-#         return torch.tensor(np_items, device=device).float()  # assigned here
-
 
