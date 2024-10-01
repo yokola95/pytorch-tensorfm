878bce2d264ed50dda5a33e0f41733590d98c7a4
viderman <viderman@yahooinc.com>
2024-10-01 09:33:44 +0100

    added fixes for the tensorfm parameters

--- src/torchfm/torch_utils/utils.py
+++ src/torchfm/torch_utils/utils.py
@@ -114,7 +114,7 @@ def get_iterators(train_dataset, valid_dataset, test_dataset, batch_size, num_wo
     return train_data_loader, valid_data_loader, test_data_loader
 
 
-def get_model(name, dataset, rank_param, emb_size, dim_int = [2,3], ten_ranks = [2,2]):
+def get_model(name, dataset, rank_param, emb_size, tensor_fm_params=None):
     """
     Hyperparameters are empirically determined, not opitmized.
     """
@@ -140,7 +140,7 @@ def get_model(name, dataset, rank_param, emb_size, dim_int = [2,3], ten_ranks = 
     # dim_int = [d_1,...,d_l] and rank_tensors = [r_1,...,r_l] are two lists
     # For an index 1 <= i <= l, we consider a d_i-order interaction of rank r_i
     elif name == 'tensorfm':
-        return TensorFactorizationMachineModel(num_features, num_columns, emb_size, dim_int=dim_int, rank_tensors=ten_ranks, is_multivalued=is_multival)
+        return TensorFactorizationMachineModel(num_features, num_columns, emb_size, dim_int=tensor_fm_params.dim_int, rank_tensors=tensor_fm_params.ten_ranks, is_multivalued=is_multival)
     elif name == 'fnn':
         return FactorizationSupportedNeuralNetworkModel(num_features, embed_dim=16, mlp_dims=(16, 16), dropout=0.2)
     elif name == 'wd':
