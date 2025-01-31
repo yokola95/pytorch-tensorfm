95d716e1eb83ef9353f28eef965047683d5bc384
alessio <alessiomazzetto1995@gmail.com>
2025-01-31 10:44:56 -0500

    added regularization linear dcn

--- src/torchfm/model/dcn.py
+++ src/torchfm/model/dcn.py
@@ -1,6 +1,6 @@
 import torch
 
-from src.torchfm.layer import FeaturesEmbedding, CrossNetwork, MultiLayerPerceptron
+from src.torchfm.layer import FeaturesEmbedding, CrossNetwork, MultiLayerPerceptron, FeaturesLinear
 
 
 class DeepCrossNetworkModel(torch.nn.Module):
@@ -17,7 +17,8 @@ class DeepCrossNetworkModel(torch.nn.Module):
         self.embed_output_dim = len(field_dims) * embed_dim
         self.cn = CrossNetwork(self.embed_output_dim, num_layers)
         #self.mlp = MultiLayerPerceptron(self.embed_output_dim, mlp_dims, dropout, output_layer=False)
-        self.linear = torch.nn.Linear(self.embed_output_dim, 1)
+        # self.linear = torch.nn.Linear(self.embed_output_dim, 1)
+        self.linear = FeaturesLinear(field_dims, is_multival=is_multivalued)
         #self.linear = torch.nn.Linear(mlp_dims[-1] + self.embed_output_dim, 1)
 
     def forward(self, x):
@@ -26,9 +27,9 @@ class DeepCrossNetworkModel(torch.nn.Module):
         """
         embed_x, reg_emb = self.embedding(x)
         embed_x = embed_x.view(-1, self.embed_output_dim)
-        x_l1 = self.cn(embed_x)
+        x_l1, reg_cn = self.cn(embed_x)
         #h_l2 = self.mlp(embed_x)
         #x_stack = torch.cat([x_l1, h_l2], dim=1)
         x_stack = torch.cat([x_l1], dim=1)
-        p = self.linear(x_stack)
-        return torch.sigmoid(p.squeeze(1))
+        p, reg_lin = self.linear(x_stack)
+        return torch.sigmoid(p.squeeze(1)), [reg_emb,reg_cn + reg_lin]
